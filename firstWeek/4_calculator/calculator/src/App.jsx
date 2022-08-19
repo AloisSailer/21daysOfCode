1f724b47c0a44ea8d4ca96e4ea97925b0c057bfc
Alois Sailer <alois_sailer@yahoo.com.br>
2022-08-19 17:56:36 -0300

    criando calculatora

---
+++ firstWeek/4_calculator/calculator/src/App.jsx
@@ -0,0 +1,32 @@
+import "./style.css"
+
+function App() {
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">123,345</div>
+        <div className="current-operand">567,765</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+    </div>
+  )
+}
+
+export default App
