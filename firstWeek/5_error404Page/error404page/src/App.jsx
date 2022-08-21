78872b8983f92adf759556bc4bc3c97d30d5b840
Alois Sailer <alois_sailer@yahoo.com.br>
2022-08-20 22:46:56 -0300

    página criada

---
+++ firstWeek/5_error404Page/error404page/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Procura-se</h1>
+      <img src="../img/cachorro.jpg" alt="cachorro beagle" />
+      <h2>404 - página não encontrada</h2>
+    </div>
+  )
+}
+
+export default App
