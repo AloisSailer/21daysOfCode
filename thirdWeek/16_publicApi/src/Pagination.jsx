81105395d1f2cfd4030f1bfc8e9d9e41b18a20f5
Alois Sailer <alois_sailer@yahoo.com.br>
2022-09-01 20:20:17 -0300

    button alterado

--- thirdWeek/16_publicApi/src/Pagination.jsx
+++ thirdWeek/16_publicApi/src/Pagination.jsx
@@ -3,8 +3,8 @@ import React from "react"
 export default function Pagination({ gotoNextPage, gotoPrevPage }) {
   return (
     <div>
-      {gotoPrevPage && <button onClick={gotoPrevPage}>Previous</button>}
-      {gotoNextPage && <button onClick={gotoNextPage}>Next</button>}
+      {gotoPrevPage && <button onClick={gotoPrevPage}>Anterior</button>}
+      {gotoNextPage && <button onClick={gotoNextPage}>Próxima</button>}
     </div>
   )
 }
